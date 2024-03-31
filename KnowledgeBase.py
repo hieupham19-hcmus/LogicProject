baf23557ed49f9f759fc51072975ddc105319d8b
Pham Ha Hieu <hieuphamha19@gmail.com>
2024-03-31 20:29:16 +0700

    initial code

---
+++ KnowledgeBase.py
@@ -0,0 +1,11 @@
+class KnowledgeBase:
+    def __init__(self):
+        self.sentences = []
+
+    def add_sentence(self, sentence_str):
+        sentence = sentence_str.split(' OR ')
+        self.sentences.append(sentence)
+
+    def size(self):
+        return len(self.sentences)
+
