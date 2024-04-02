f95d1812d148dca8edddd747e2176088f1c6bcfb
Pham Ha Hieu <hieuphamha19@gmail.com>
2024-04-02 20:57:36 +0700

    redirect file

--- KnowledgeBase.py
+++
@@ -1,7 +0,0 @@
-class KnowledgeBase:
-    def __init__(self):
-        self.sentences = []
-
-    def add_sentence(self, sentence_str):
-        sentence = sentence_str.split(' OR ')
-        self.sentences.append(sentence)
